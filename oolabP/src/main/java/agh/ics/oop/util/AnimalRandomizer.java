a4ee191bdb6dcdd1508c7f48459ac39e92559730
Jakub <njakub2005@gmail.com>
2026-01-17 15:31:39 +0100

    "Rysowanie mapy - jungla jest oddzielona"

--- oolabP/src/main/java/agh/ics/oop/util/AnimalRandomizer.java
+++ oolabP/src/main/java/agh/ics/oop/util/AnimalRandomizer.java
@@ -8,7 +8,6 @@ import java.util.Random;
 
 public  class AnimalRandomizer {
     private final Random random = new Random();
-    private final int[] genePool = {0,1,2,3,4,5,6,7};
     private final WorldDirections[] facingDirectionsPool = {
             WorldDirections.NORTH,
             WorldDirections.NORTH_EAST,
@@ -24,7 +23,7 @@ public  class AnimalRandomizer {
         for (int i = 0; i < config.animal().numberAtStart(); i++) {
             ArrayList<Integer> gene = new ArrayList<>();
             for (int j = 0; j < config.genotype().length(); j++) {
-                gene.add(random.nextInt(genePool.length));
+                gene.add(random.nextInt(8));
             }
             animals.add(new Animal(
                     new Vector2d(random.nextInt(0, config.map().width()),random.nextInt(0,config.map().height())),
